bb2c22573cc1cc1589c934e7cc3bb159b0649889
Glenn Chiang <glennchiang24@gmail.com>
2023-09-21 12:17:58 +0800

    Developed review screen layout, positioning

--- katanakana/src/components/BaseLayout.tsx
+++ katanakana/src/components/BaseLayout.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="bg-black z-10 fixed w-screen bottom-0 sm:top-0 left-0 h-12 p-4 py-8 flex items-center justify-center">
+    <nav className="bg-black shadow shadow-white z-10 fixed w-screen bottom-0 sm:top-0 left-0 h-12 p-4 py-8 flex items-center justify-center">
       <Link to={'/'}>
         <FontAwesomeIcon icon={faHome}/>
       </Link>
@@ -21,7 +21,7 @@ export default function BaseLayout({
   return (
     <>
       <Navbar />
-      <div className="mb-12">{children}</div>
+      <div className="">{children}</div>
     </>
   );
 }
--- katanakana/src/features/review/Review.tsx
+++ katanakana/src/features/review/Review.tsx
@@ -2,21 +2,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import BaseLayout from "../../components/BaseLayout";
 import { useAppSelector } from "../../store";
 import { Answer } from "../game/answersSlice";
-import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 export default function Review() {
   const answers = useAppSelector((state) => state.answers);
+
   return (
     <BaseLayout>
-    <Link to={'/dojo'} className="flex gap-2 items-center fixed p-4 z-10 bg-black">
-      <FontAwesomeIcon icon={faChevronLeft}/>
-      Back
-    </Link>
-      <section className="h-screen">
-        {answers.map((answer, index) => (
+      <Link
+        to={"/dojo"}
+        className="flex gap-2 items-center fixed right-0 p-4 z-10 bg-black top-0 w-full justify-end"
+      >
+        Next Game
+        <FontAwesomeIcon icon={faChevronRight} />
+      </Link>
+      
+      <section className="flex items-center justify-center flex-col gap-20 my-20">
+        {answers.length > 0 ? answers.map((answer, index) => (
           <KanaAnswer key={index} answer={answer} />
-        ))}
+        )): <p>No answers entered</p>}
       </section>
     </BaseLayout>
   );
@@ -24,9 +29,18 @@ export default function Review() {
 
 function KanaAnswer({ answer }: { answer: Answer }) {
   return (
-    <article className="flex flex-col h-1/3 items-center justify-center text-6xl">
-      <span>{answer.kana.kana}</span>
-      <span>{answer.kana.id}</span>
+    <article
+      className={`flex flex-col h-1/3 gap-4 items-center justify-center text-6xl ${
+        answer.correct || "text-rose-400"
+      }`}
+    >
+      <div className="flex flex-col items-center">
+        <span>{answer.kana.kana}</span>
+        <span>{answer.kana.id}</span>
+      </div>
+      {answer.correct || (
+        <span className="text-2xl text-rose-400">Skipped</span>
+      )}
     </article>
   );
 }
